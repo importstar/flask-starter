51d0fc4f5713230310a6f15b2ec2931f402bcd7b
jakeeind <jakee_ind@hotmail.com>
2025-03-17 20:58:28 +0700

    feat: 🎸 add user login

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,2 +1,26 @@
+from ..models.user_model import User
+from flask_login import login_user
+import datetime
+
+
 class UserService:
-    pass
+    @staticmethod
+    def login(username: str, password: str):
+        user = User.objects(username=username).first()
+        error_msg = ""
+        if not user or not user.check_password(password):
+            error_msg = "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง"
+
+        if user and user.status == "disactive":
+            error_msg = "บัญชีของท่านถูกลบออกจากระบบ"
+
+        if error_msg:
+            return {"error_msg": error_msg, "success": False}
+        login_user(user)
+        user.last_login_date = datetime.datetime.now()
+        user.save()
+        return {"error_msg": "", "success": True}
+
+    @staticmethod
+    def register(username: str, password: str):
+        pass
--- app/web/views/users.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-module = Blueprint("users", __name__, url_prefix="/users")
-
-
-@module.route("/login")
-def login():
-    return render_template("/users/login.html")
